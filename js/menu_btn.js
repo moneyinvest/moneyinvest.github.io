4eb2c0c880875c7a0a20bc806bfd3df6156eb7fe
moneyinvest <49990192+moneyinvest@users.noreply.github.com>
2019-09-09 18:27:09 +0300

    Add files via upload

--- js/menu_btn.js
+++ js/menu_btn.js
@@ -1,27 +1,34 @@
 $(document).ready(function () {
     
-        $(window).scroll(function() {
-            // проверка на докрутку до определенного элемента
-           
-            var scroll_picca =$('.about_us').offset().top;
-            //если мы докрутили до нужного элемента 
-            ;
-            if ($(this).scrollTop() > scroll_picca) {
-              // создаем эффекты и анимацию
-              $('.navbar-menu').fadeOut(1000);
-              $('.navbar-social').fadeOut(1000);
-              $('.navbar-menu_btn').css('display','block');
-              $('.navbar-logo').css('display','block');
-            }
-            
+      if(window.innerWidth<500)
+      {
+        $('.navbar-menu ul').css('display','block');
+        $('.menu_link').addClass('menu_link2');
+        $('.menu_link2').removeClass('menu_link');
         
-        }); 
-        $('.navbar-menu_btn').click(function()
-        {
-            $(this).css('display','none');
-           $('.navbar-menu').fadeIn(1000);
-           
-           $('.navbar-logo').css('display','none');
-        })
-    
+        $(window).scroll(function() {
+          // проверка на докрутку до определенного элемента
+         
+          var scroll_picca =$('.about_us').offset().top;
+          //если мы докрутили до нужного элемента 
+          ;
+          if ($(this).scrollTop() > scroll_picca) {
+            // создаем эффекты и анимацию
+            $('.navbar-menu').fadeOut(1000);
+            $('.navbar-social').fadeOut(1000);
+            $('.navbar-menu_btn').css('display','block');
+            $('.navbar-logo').css('display','block');
+          }
+          
+      
+      }); 
+      $('.navbar-menu_btn').click(function()
+      {
+          $(this).css('display','none');
+         $('.navbar-menu').fadeIn(1000);
+         
+         $('.navbar-logo').css('display','none');
+      })
+  
+      }
 });
